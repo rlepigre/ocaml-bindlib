3f816be90654a916a620ec1aaffc3f869314448b
craff <christophe@raffalli.eu>
2023-04-05 00:35:29 +0200

    Replace earley by pacomb in examples.

--- examples/metavar/parser.ml
+++ examples/metavar/parser.ml
@@ -1,30 +1,32 @@
-open Earley_core
+open Pacomb
 open Ast
 
-let parser ident = id:{#[a-z0-9][_a-zA-Z0-9]*[']*#}[group.(0)]
+let%parser ident = (id::RE"[a-z0-9][_a-zA-Z0-9]*[']*") => id
 
-let parser term_atom =
-  | id:ident            -> PVar id
-  | '(' t:term_full ')' -> t
-  | '[' t:term_full ']' -> PNrm t
+let%parser rec term_atom =
+    (id::ident)            => PVar id
+  ; '(' (t::term_full) ')' => t
+  ; '[' (t::term_full) ']' => PNrm t
 
-and parser term_appl =
-  | term_atom
-  | t:term_appl u:term_atom -> PApp(t,u)
+and term_appl =
+    (t::term_atom) => t
+  ; (t::term_appl) (u::term_atom) => PApp(t,u)
 
-and parser term_full =
-  | term_appl
-  | {"λ"|"%"} ids:ident* '.' t:term_full -> many_PLam ids t
+and term_full =
+    (t::term_appl) => t
+  ; ("λ" => () ; "%" => ()) (ids:: ~* ident) '.' (t::term_full)
+      => many_PLam ids t
 
-let parser command =
-  | "$u"                         -> Undo
-  | "$g"                         -> Goal
-  | "$s" x:ident "=" t:term_full -> Decl(x,t)
-  | "$p" t:term_full             -> Prnt(t)
+let%parser command =
+    "$u"                               => Undo
+  ; "$g"                               => Goal
+  ; "$s" (x::ident) "=" (t::term_full) => Decl(x,t)
+  ; "$p" (t::term_full)                => Prnt(t)
 
-let parser commands = {command ";"}*
+let%parser commands = cs :: ~* ((c::command) ";" => c) => cs
+
+let blank = Blank.line_comments "//"
 
 let parse_channel : in_channel -> cmd list = fun ic ->
-  let blank = Blanks.line_comments "//" in
-  let parse = Earley.parse_buffer commands blank in
-  Earley.handle_exception parse (Input.from_channel ic)
+  let parse = Grammar.parse_channel commands blank in
+  Pos.handle_exception parse ic
